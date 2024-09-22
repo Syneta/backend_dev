fc343dd599240a80a8fdecb8834560c65a44c95d
Syneta <144949309+Syneta@users.noreply.github.com>
2024-09-22 19:04:36 +0000

    skil
    fyrirlestur og skilaverkefni 1

---
+++ csharp_primer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Console.Write("Hæhæ, hvað heitirðu? ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hæ {name}");
+Console.Write("Hvaða módel ertu? ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine($"Snilld, þú ert {age} módel, \nég er reyndar 24 módel");
+*/
+
+for(int i = 0; i < 10; i++)
+{
+    if(i == 6)
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
